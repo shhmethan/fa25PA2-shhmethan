be73a7d70533e0c535d64b481f8ea7cdc4edba29
Ethan <ebrothers7061@sdsu.edu>
2025-11-03 21:20:25 -0800

    Junk Commit + push all commits

---
+++ cmake-build-debug/CMakeFiles/fa25PA2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fa25PA2.dir/main.cpp.obj"
+  "CMakeFiles/fa25PA2.dir/main.cpp.obj.d"
+  "fa25PA2.exe"
+  "fa25PA2.exe.manifest"
+  "fa25PA2.pdb"
+  "libfa25PA2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fa25PA2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
